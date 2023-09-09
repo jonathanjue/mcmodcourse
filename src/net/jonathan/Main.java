73d2c5b8f83f95eb253226dcf93e4b3e5accd558
Jonathan Jue <jj@phy6.net>
2023-09-09 12:50:23 -0400

    first commit

---
+++ src/net/jonathan/Main.java
@@ -0,0 +1,193 @@
+package net.jonathan;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+{
+    {
+//
+//
+//        int score = 0;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("what is the most used letter");
+//        String input1 = scanner.next();
+//
+//
+//        if (input1.equals("e")) {
+//            System.out.println("Correct");
+//            score++;
+//
+//        }
+//        else {
+//        System.out.println("your wrong");
+//        System.out.println("Is's e");
+//        }
+//
+//
+//        System.out.println("what is the most common word");
+//        String input2 = scanner.next();
+//
+//
+//        if (input2.equals("The")) {
+//            System.out.println("Correct");
+//            score++;
+//        }
+//        else {
+//            System.out.println("your wrong");
+//            System.out.println("It is The");
+//        }
+//
+//        System.out.println("what dose water taste like");
+//        String input3 = scanner.next();
+//
+//
+//        if (input3.equals("Nothing")) {
+//            System.out.println("Correct");
+//            score++;
+//        }
+//        else {
+//            System.out.println("your wrong");
+//            System.out.println("It is Nothing");
+//        }
+//        System.out.println("your score is :" + score);
+//    }
+//}
+
+//            int exam2 = 51 ;
+//        }
+//            int medalPlacement = 1;
+//
+//            switch(medalPlacement) {
+//                    case 1: System.out.println("GOLD MEDAL"); break;
+//                    case 2: System.out.println("SILVER MEDAL"); break;
+//                    case 3: System.out.println("BRONZE MEDAL"); break;
+//                    default: System.out.println("NO MEDAL"); break;
+//            }
+//        }
+//}
+//
+//
+//            int exam1 = 65;
+//
+//                Scanner scanner = new Scanner((System.in));
+//                System.out.println("Results for Exam !:  ");
+//                exam1 = scanner.nextInt();
+//                System.out.println("Results for Exam 2:  ");
+//
+//            if(exam1 >= 50) {
+//                System.out.println("You have passed Exam 1");
+//            } else {
+//                    System.out.println("You have Failed Exam 1");
+//            }            if(exam2 >= 50) {
+//                        System.out.println("You have passed Exam 2");
+//                } else {
+//                        System.out.println("You have Failed Exam 2");
+//                }
+//
+//
+//
+//                if(exam1 >= 50 && exam2 >= 50) {
+//                    System.out.println("You have passed the CLASS");
+//
+//        }
+//}}
+//
+//
+//
+
+//        /* BOOLEAN AND COMPARISON OPERATORS */
+//        boolean likesTheLectures = true;
+//        boolean askedQuestion = false;
+//
+//
+//        System.out.println(("You like these lectures " + likesTheLectures));
+//        System.out.println(("You have asked a Question in the Q&A " + askedQuestion));
+//
+//        // COMPARISON OPERATORS
+//        // > >= == != < <=
+//
+//        int yourScore = 98;
+//
+//        boolean passedClass = yourScore >= 50;
+//        System.out.println("You passed:  " + passedClass + " with " + yourScore + " points!");
+//
+//        boolean hasPerfectScore = yourScore == 100;
+//        boolean failedClass !passedClass;
+//
+//
+//        }
+//}
+//
+//
+
+//System.out.println();
+//
+//        int x = 100;
+//        int y = 20;
+//
+//        x = x + y;
+//        x += y;
+//        System.out.println(x);
+//        x -= y;
+//        System.out.println(x);
+//        x *= y;
+//        System.out.println(x);
+//        x /= y;
+//        System.out.println(x);
+//        x %= y;
+//        System.out.println(x);
+//
+//        // Incrementing
+//        x = x + 1;
+//        x += 1;
+//        x++;
+//        System.out.println(x);
+//
+//        // Decrementing
+//        x--;
+//        System.out.println(x);
+
+//        int x = 100;
+//        int y = 20;
+//        int result;
+//
+//         Addition
+//        result = x + y;
+//        System.out.println(result);
+//
+//         Subtraction
+//        result = x - y;
+//        System.out.println(result);
+//
+//         Multiplication
+//        result = x * y;
+//        System.out.println(result);
+//
+//         Division
+//        result = x / y;
+//        System.out.println(result);
+//
+//         Remainder
+//        result = 20 % 2;
+//        System.out.println(result);
+//
+//         MATH METHODS
+//        System.out.println((Math.max(x, y)));
+//        System.out.println((Math.abs(-500)));
+//
+//
+//
+//        System.out.println("Hello world!");
+//
+//        int x = 10;
+//        System.out.println(x);
+//
+//
+//        System.out.println("Outputting some Value: " + x);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Type in your Username");
+//        String input = scanner.next();
+//
+//        System.out.println("Your Username is: " + input);
